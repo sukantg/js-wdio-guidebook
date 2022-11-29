0a7661afaadf0f79fbc6848917d56002ff3900e7
Sukant Ghosh <sukantghosh@Sukants-MacBook-Air-2.local>
2022-11-29 11:48:58 +0100

    Adding page objects

--- test/specs/login.js
+++ test/specs/login.js
@@ -1,23 +1,50 @@
+
+class Auth{
+    get $email () { return $('input[type="email"]') }
+    get $password () { return $('input[type="password"]') }
+    get $signIn () { return $('button*=Sign in') }
+    get $errorMessages () { return $('.error-messages li')}
+}
+
+const auth = new Auth();
+
+
+function login(username, password){
+    auth.$email.setValue(email);
+    auth.$password.setValue(password);
+    auth.$signIn.click();
+}
 // Login Page
 
 describe ('Login Page', function(){
 
+    beforeEach(function(){
+        browser.url('./login');
+    })
+
+    // Login works with correct username and password
     it('should let you login', function(){
+        login('demo@learnwebdriver.com','wdiodemo');
+        auth.$signIn.waitForExist({reverse : true});
+        expect(auth.$errorMessages).not.toBeExisting();
+    })
 
-        $('input[type="email"]').setValue('demo@learnwebdriver.io');
-        $('input[type="password"]').setValue('wdiodemo');
-        $('button=Sign in').click();
+    // Login fails with incorrect username
+    it('should fail without password', function(){
+       login('','wdiodemo');
+       expect(auth.$errorMessages).toHaveText('Email can\'t be blank');
+    })
 
-        // expect($('.error-messages li')).not.toBeExisting();
-
-        $('=Settings').waitForExist();
-        expect(browser.getUrl()).not.toContain('/login');
+    // Login fails with incorrect password
+    it('should fail without username', function(){
+       login('demo@learnwebdriver.com','');
+       expect(auth.$errorMessages).toHaveText('Password can\'t be blank');
     })
 })
 
-// Login works with correct username and password
 
-// Login fails with incorrect username
 
-// Login fails with incorrect password
+
+
+
 
